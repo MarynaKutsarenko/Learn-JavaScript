bc86ea61e1d78c8048d157578b59a67629b30e67
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2020-03-26 19:16:37 +0200

    добавила let к каждой пересенной

--- script/main.js
+++ script/main.js
@@ -1,9 +1,9 @@
-let money,
-  income,
-  addExpenses,
-  deposit,
-  mission,
-  period;
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
 
 alert ('Learn JavaScript!');
 
