53b63b1de929af552aeb6eb84ecf6dafec52306d
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2020-03-27 19:51:55 +0200

    Первичный кодинг по кроку 2

--- script/main.js
+++ script/main.js
@@ -1,10 +1,19 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission,
-    period;
+let money = 5000,
+    income = 'фриланс',
+    addExpenses = 'тренажерка Интернет кваРтплата',
+    deposit = true,
+    mission = 80000,
+    period = 7,
+    budgetDay = money / 30;
 
-alert ('Learn JavaScript !');
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
 
-console.log('I am glad to be here');
+console.log(addExpenses.length);
+
+console.log('Период равен ', period , ' месяцнв, ' , ' Цель заработать ', mission, 'рублей/долларов/гривен/юани ');
+
+console.log(addExpenses.toLowerCase().split(' '));
+
+console.log(Math.round(budgetDay));
