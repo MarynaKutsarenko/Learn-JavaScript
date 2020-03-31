6521d840966f275eb38402d542fed94d50ac1936
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2020-03-31 22:25:03 +0300

    тренинг функций

--- script/main.js
+++ script/main.js
@@ -1,9 +1,31 @@
 'use strict'
 
-let name = +prompt('Введите сторону квадрата');
-let name2 = +prompt('Введите сторону ');
+function one(callback) {
+  console.log('сделать запрос');
+  setTimeout(function(){
+    console.log('получить данные');
+  }, 1000);
+  setTimeout(function(){
+    callback();
+  }, 2000);
+}
 
-let res = name * name2;
+
+one(two);
 
 
-console.log(res); 
+function two() {
+  console.log('отобразить');
+}
+
+//детерминированная функция зависит от данных
+
+function res(w, e) {
+  const sum = w * e;
+  return sum;
+}
+
+console.log(res(3, 4));
+console.log(res(6, 4));
+console.log(res(7, 8));
+console.log(res(8, 4));
