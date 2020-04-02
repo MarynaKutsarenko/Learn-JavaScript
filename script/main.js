f49ad779d407ef71c81e6612bc6b3bccc73ce13a
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2020-04-02 20:54:28 +0300

    просто тесты

--- script/main.js
+++ script/main.js
@@ -1,31 +1,65 @@
 'use strict'
 
-function one(callback) {
-  console.log('сделать запрос');
-  setTimeout(function(){
-    console.log('получить данные');
-  }, 1000);
-  setTimeout(function(){
-    callback();
-  }, 2000);
-}
+// function test(data) {
+//   data--;
+//   console.log(data);
+
+//   if (data>0) {
+//     test(data);
+//   }
+// }
+// test(10);
+
+let n=5;
+let m = [];
+// while (n < 6) {
+//   console.log(n);
+//   n++;
+// }
+
+// do {
+//   m += +prompt('вопрос');
+//   ++n;
+// } while (n < 10) {
+//   console.log(m);
+// };
+
+// for (let i = 0; i < 5; ++i){
+//   if (i === 2) {
+//     break;
+//   }
+//   console.log(i);
+// }
 
 
-one(two);
 
 
-function two() {
-  console.log('отобразить');
-}
+// function one(callback) {
+//   console.log('сделать запрос');
+//   setTimeout(function(){
+//     console.log('получить данные');
+//   }, 1000);
+//   setTimeout(function(){
+//     callback();
+//   }, 2000);
+// }
 
-//детерминированная функция зависит от данных
 
-function res(w, e) {
-  const sum = w * e;
-  return sum;
-}
+// one(two);
 
-console.log(res(3, 4));
-console.log(res(6, 4));
-console.log(res(7, 8));
-console.log(res(8, 4));
+
+// function two() {
+//   console.log('отобразить');
+// }
+
+// //детерминированная функция зависит от данных
+
+// function res(w, e) {
+//   const sum = w * e;
+//   return sum;
+// }
+
+// console.log(res(3, 4));
+// console.log(res(6, 4));
+// console.log(res(7, 8));
+// console.log(res(8, 4));
