cce7c277605350a61412a73c3ee32d87891167e5
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2020-04-23 19:54:57 +0300

    выполнила доп

--- script/main.js
+++ script/main.js
@@ -1,10 +1,45 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission,
-    period;
+let lang = 'en';
 
-alert ('Learn JavaScript !');
+let dayOfWeeksRu = ["'Понедельник',  'Вторник', 'Среда', 'Четверг',  'Пятница',  'Суббота',  'Воскресенье'" ];
+let dayOfWeeksEng = ["'Monday',  'Tuesday',  'Wednesday',  'Thursday',  'Friday',  'Saturday',  'Sunday'" ];
 
-console.log('I am glad to be here');  
+let arr = [
+  ["'Понедельник',  'Вторник', 'Среда', 'Четверг',  'Пятница',  'Суббота',  'Воскресенье'" ],
+  ["'Monday',  'Tuesday',  'Wednesday',  'Thursday',  'Friday',  'Saturday',  'Sunday'" ]
+]
+
+console.log(arr[0]);
+
+if (lang == 'ru') {
+  console.log(dayOfWeeksRu);
+} else if (lang == 'en') {
+  console.log(dayOfWeeksEng);
+} else {
+  console.log('Извините! такой язык мы не поддерживаем');
+}
+
+switch (lang) {
+  case 'ru':
+    console.log(dayOfWeeksRu);
+  break;
+  case 'en':
+    console.log(dayOfWeeksEng);
+  break;
+  default:
+    console.log('Извините! такой язык мы не поддерживаем');
+  break;
+}
+
+let nameParson = 'Максим ';
+
+nameParson == 'Артем' ? (
+  console.log('директор')
+) : (
+  console.log('Максим'),
+  console.log('преподаватель')
+) 
+
+let name = 'Артем' ;
+
+name == 'Артем' ? console.log('директор') : console.log('студент');
+
