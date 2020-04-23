891e6fbba3bfcc9e130546adf6f18bc63ce3be15
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2020-04-23 17:04:21 +0300

    урок завершен

--- script/main.js
+++ script/main.js
@@ -1,102 +1,149 @@
-document.addEventListener('DOMContentLoaded', function(){
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.todo-control');
+  const input = document.querySelector('.header-input');
+  const todoList = document.getElementById('todo');
+  const completedList = document.getElementById('completed');
 
-  const todoControl = document.querySelector('.todo-control'),
-    headerInput = document.querySelector('.header-input'),
-    todoList = document.querySelector('.todo-list'),
-    todoCompleted = document.querySelector('.todo-completed'),
-    headerButton = document.querySelector('.header-button'),
-    addHeaderButton = document.getElementById('add'),
-    parrentItem = document.querySelector('.todo-item'),
-    todoContainer = document.querySelector('.todo-container'),
-    todoComplete = document.querySelector('.todo-completed');
+  // обьект, который будет содержать данные
+  let data = {
+      todo: [],
+      completed: []
+  }
 
-  let  todoData = [];
+  // получаем данные из localstorage если они есть
+  if (localStorage.getItem('todoList')) {
+      data = JSON.parse(localStorage.getItem('todoList'))
+  }
 
-    const render = function() {
-      if (localStorage.getItem('newTodo')) {
-      todoData = JSON.parse(localStorage.getItem('newTodo'));
+  // функция сохранения элемента в localStorage
+  const dataObjectUpdated = () => {
+      localStorage.setItem('todoList', JSON.stringify(data));
+  }
+
+  // функция отрисовывает элементы при загрузке страницы, если обьект не пустой
+  const renderTodoList = () => {
+      if (!data.todo.length && !data.completed.length) return;
+
+      data.todo.forEach((item) => {
+          renderItem(item);
+      })
+
+      data.completed.forEach((item) => {
+          renderItem(item, true);
+      })
+  }
+
+  // функция, которая добавляет данные в обьект и запускает рендер
+  const addElement = (text) => {
+      renderItem(text);
+      input.value = '';
+      data.todo.push(text);
+      dataObjectUpdated();
+  }
+
+  // функция удаления
+  const removeItem = function () {
+      // получаем весь элемент li, которому принадлежит кликнутая кнопка, его родителя, id и текст элемента
+      const item = this.parentNode.parentNode;
+      const parent = item.parentNode;
+      const id = parent.id;
+      const text = item.textContent;
+
+      // в зависимости от id контейнера - удаяем элемент из обьекта
+      if (id === 'todo') {
+          data.todo.splice(data.todo.indexOf(text), 1);
       } else {
-      todoData = [];
+          data.completed.splice(data.completed.indexOf(text), 1);
       }
 
-      todoList.textContent = '';
-      todoCompleted.textContent = '';
+      // сохраняем в localStorage
+      dataObjectUpdated();
 
-      todoData.forEach(function (item) {
+      // в зависимости от id контейнера - удаяем элемент со страницы
+      parent.removeChild(item);
+  }
 
-        const li = document.createElement('li');
-          li.classList.add('todo-item');
-            li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
-            '<div class="todo-buttons">' +
-              '<button class="todo-remove"></button>' +
-              '<button class="todo-complete"></button>' +
-            '</div>';
+  // функция выполнения
+  const completeItem = function () {
+      // получаем весь элемент li, которому принадлежит кликнутая кнопка, его родителя, id и текст элемента
+      const item = this.parentNode.parentNode;
+      const parent = item.parentNode;
+      const id = parent.id;
+      const text = item.textContent;
+      let target;
 
-          if (item.completed) {
-            todoCompleted.append(li);
-          } else {
-            todoList.append(li);
-          }
+      // определяем цель в которую будем перемещать элемент
+      if (id === 'todo') {
+          target = document.getElementById('completed');
+      } else {
+          target = document.getElementById('todo');
+      }
 
-        const btnTodoComleted = li.querySelector('.todo-complete');
-          btnTodoComleted.addEventListener('click', function(){
-            item.completed = !item.completed;
-              
-            localStorage.setItem('newTodo', JSON.stringify(todoData));
-            render();
-          });
+      // в зависимости от id контейнера: если id=todo - перемещаем в completed, или наоборот
+      if (id === 'todo') {
+          data.todo.splice(data.todo.indexOf(text), 1);
+          data.completed.push(text);
+      } else {
+          data.completed.splice(data.completed.indexOf(text), 1);
+          data.todo.push(text);
+      }
 
-        const btnRemove = document.getElementsByClassName('todo-remove');
-          btnRemove.addEventListener('click', function(event){
-            btnRemove.forEach(elem, function () {
-              
-            });
-            console.log(li);
-            console.log(event.target);
+      // удаляем элемент со тсраницы из родителя и вставляем его в цель target
+      parent.removeChild(item);
+      target.insertBefore(item, target.childNodes[0]);
 
-          });
-      });
-    
-    };
+      // сохраняем в localStorage
+      dataObjectUpdated();
+  }
 
-  todoControl.addEventListener('submit', function (event){
-    event.preventDefault(); 
+  // функция рендер
+  const renderItem = (text, completed = false) => {
+      let list;
 
-    const newTodo = {
-      value: headerInput.value,
-      completed: false
-    };
+      // определяем чем является лист
+      if (completed) {
+          list = completedList;
+      } else {
+          list = todoList
+      }
 
-    if (headerInput.value !== '' ) {
-      addHeaderButton.removeAttribute('disabled');
-      render(headerInput.value);
-      todoData.push(newTodo);
-    } else {
-      alert('заполните поле'); 
-    }
+      // создаем будущие элементы нашего Item
+      const item = document.createElement('li');
+      const buttons = document.createElement('div');
+      const remove = document.createElement('button');
+      const complete = document.createElement('button');
 
-    headerInput.value = '';
-    localStorage.setItem('newTodo', JSON.stringify(todoData));
-    render();
-    
-  });
-  render();
-  
-});
+      // даем новым элементам классы
+      item.className = 'todo-item';
+      buttons.classList.add('todo-buttons');
+      remove.classList.add('todo-remove');
+      complete.classList.add('todo-complete');
 
+      // заполняем текстом
+      item.textContent = text;
 
-  // const itemRemove = function(li){
-  //   const itemTodo = todoList;
-  //   const itemCompleted = todoCompleted;
-  //   console.log(itemTodo);
-  //   console.log(itemCompleted);
-  //   }
+      // вешаем обработчики
+      remove.addEventListener('click', removeItem);
+      complete.addEventListener('click', completeItem);
 
-// const shouldRemov = localStorage.getItem('newTodo');
-    // document.addEventListener('keyup', function(event){
-  //   if (event.keyCode === 13) {
-  //     todoData.push(newTodo);
-  //     headerInput.value = '';
-  //   }
-  // });
+      // собираем блок buttons и после блок Item
+      buttons.appendChild(remove);
+      buttons.appendChild(complete);
+      item.appendChild(buttons);
+
+      // добавляем на страницу
+      list.insertBefore(item, list.childNodes[0]);
+  }
+
+  // вешаем на форму обработчик событий, который будет отрабатывать, если текст есть
+  form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      if (input.value) {
+          addElement(input.value);
+      }
+  })
+
+  // запускаем функцию проверки наполненности обьекта
+  renderTodoList()
+})
