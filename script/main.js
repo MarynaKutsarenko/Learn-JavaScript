fc6f3fb5c90b9e489132ca96f0143cd107abdb02
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2020-04-29 15:34:58 +0300

    не добавляется цвет фона

--- script/main.js
+++ script/main.js
@@ -1,10 +1,37 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission,
-    period;
+'use strict'
+function DomElement(selector,bg, height, width, fontSize) {
+  this.selector =  selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+};
 
-alert ('Learn JavaScript !');
+DomElement.prototype.buildElements = function(){
+  let element;
 
-console.log('I am glad to be here');  
+  if (this.selector[0].startsWith('.')) {
+    element = document.createElement('div');
+    element.className = 'block';
+    element.textContent = 'Текст div';
+  } else if (this.selector[0].startsWith('#')){
+    element = document.createElement('p');
+    element.id = 'best';
+    element.textContent = 'Текст p';
+  } else {
+    element= document.createElement('p');
+    element.textContent = 'Текст p';
+  }
+  element.style.cssText = `height: ${this.height}; width: ${this.width}; bg: ${this.bg}; font-size: ${this.fontSize}`;
+
+  document.body.append(element);
+  document.body.style.backgroundColor = '#E0FFFF';
+};
+
+let domElementClass = new DomElement('.block', 'GreenYellow', '200px', '300px', '20px');
+let domElementId = new DomElement('#best', 'MidnightBlue', '200px', '300px', '40px');
+
+domElementClass.buildElements();
+domElementId.buildElements();
+console.log(domElementClass);
+console.log(domElementId);
