9c40d24ae590370115b978c95bc55fb5f17a526e
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2020-05-26 21:20:42 +0300

    start of lesson

--- script/main.js
+++ script/main.js
@@ -2,19 +2,31 @@ window.addEventListener('DOMContentLoaded', function(){
   'use strict';
 
   document.body.style.backgroundColor = 'yellowGreen';
+
   let date = new Date();
+
   const weekArray = ['Понедельник', 'Вторник','Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']; 
-  
+  let todaysDay = document.createElement('div');
+  let time = document.createElement('div');
+
 
   function getTime() {
 
      let hours = date.getHours(),
-        minutes = date.getMinutes(),
-        seconds = date.getSeconds(),
-        day = date.getDay();
+         minutes = date.getMinutes(),
+         seconds = date.getSeconds(),
+         day = date.getDay();
         
     day = weekArray[day];
-        console.log(day);
+
+    todaysDay.innerHTML = 'Сегодня :' + ' ' + day;
+    document.body.append(todaysDay);
+    
+    setInterval(function(){
+      time.innerHTML = ('Текущее время :' + ' ' + addZero(hours) + ':' + addZero(minutes) + ':' + addZero(seconds) + ' ' + 'PM');
+      document.body.append(time);
+    },1000);
+
   }
 
   getTime();
